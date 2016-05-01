eff60190ad61fa5b22af89f7eef59eae089fd9de
Ketan Shah <ketan.shah@gmail.com>
2016-05-01 18:18:52 +0530

    mamp start from command line using functions

---
+++ mamp.sh
@@ -0,0 +1,12 @@
+brew tap homebrew/homebrew-php
+brew install php54
+brew unlink php54
+brew install php55
+brew unlink php55
+brew install php56
+brew unlink php56
+brew install php70
+
+
+brew install nginx
+
